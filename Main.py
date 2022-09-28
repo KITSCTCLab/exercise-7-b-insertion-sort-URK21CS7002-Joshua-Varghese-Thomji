46cd401bd2551e259334507cdee687bdbfbec252
Joshua-Varghese-KITS <109643810+URK21CS7002-Joshua-Varghese-Thomji@users.noreply.github.com>
2022-09-28 11:27:12 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ def insertionSort(array) -> List[int]:
   for i in range(1, n):
     key = array[i]
     j = i-1
-    while j>=0 and A[j]>key:
+    while j>=0 and array[j]>key:
       array[j+1] = array[j]
       j-=1
     array[j+1] = key
